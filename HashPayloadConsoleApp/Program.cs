cc0c440748050d2e246f08dcc0b7628d4264961d
Stefan Vettiger <stefan.vettiger@avm.swiss>
2023-04-17 13:47:33 +0200

    short signature

--- HashPayloadConsoleApp/Program.cs
+++ HashPayloadConsoleApp/Program.cs
@@ -8,14 +8,14 @@ namespace HashPayloadConsoleApp
         static void Main(string[] args)
         {
             string payload = "Hello, world!"; // Your arbitrary payload
-            RSAParameters publicKeyParameters;
+            ECParameters publicKeyParameters;
             byte[] payloadBytes;
-            byte[] signatureBytes;
+            string base64SignatureBytes;
 
             Console.WriteLine($"Original Payload: {payload}");
 
             // Generate a new RSA key pair
-            using (RSA rsa = RSA.Create())
+            using (var rsa = ECDsa.Create(ECCurve.NamedCurves.brainpoolP160r1))
             {
                 // Get the public and private key as byte arrays
                 publicKeyParameters = rsa.ExportParameters(false);
@@ -27,14 +27,17 @@ namespace HashPayloadConsoleApp
                 byte[] hashBytes = GetPayloadHash(payloadBytes);
 
                 // Sign the hash using the private key
-                signatureBytes = rsa.SignHash(hashBytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+                byte[] signatureBytes = rsa.SignHash(hashBytes);
+                                
+                base64SignatureBytes = Convert.ToBase64String(signatureBytes);
+                Console.WriteLine(base64SignatureBytes);
             }
 
             // change payload
             payloadBytes = TryHackPayload(payloadBytes);
 
             // verify the payload -> check the signature (integrity, authentication)
-            VerifyHash(payloadBytes, signatureBytes, publicKeyParameters);
+            VerifyHash(payloadBytes, base64SignatureBytes, publicKeyParameters);
         }
 
         private static byte[] TryHackPayload(byte[] payloadBytes)
@@ -46,27 +49,22 @@ namespace HashPayloadConsoleApp
 
         private static byte[] GetPayloadHash(byte[] payloadBytes)
         {
-            byte[] hashBytes;
-            using (SHA256 sha256 = SHA256.Create())
-            {
-                hashBytes = sha256.ComputeHash(payloadBytes);
-            }
-
-            return hashBytes;
+            return MD5.HashData(payloadBytes);
         }
 
-        private static void VerifyHash(byte[] payloadBytes, byte[] signatureBytes, RSAParameters parameters)
+        private static void VerifyHash(byte[] payloadBytes, string base64SignatureBytes, ECParameters parameters)
         {
-            using (RSA rsa = RSA.Create(parameters))
+            using (var rsa = ECDsa.Create(parameters))
             {
                 // Compute the hash of the payload using SHA256
                 byte[] hashBytes = GetPayloadHash(payloadBytes);
 
                 // verify the signature using the public key
-                bool signaturevalid = rsa.VerifyHash(hashBytes, signatureBytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+                var signatureBytes = Convert.FromBase64String(base64SignatureBytes);
+                bool signaturevalid = rsa.VerifyHash(hashBytes, signatureBytes);
 
                 Console.WriteLine("signature is valid: {0}", signaturevalid);
-                
+
                 var payload = Encoding.UTF8.GetString(payloadBytes);
                 Console.WriteLine($"Received Payload: {payload}");
                 Console.Read();
