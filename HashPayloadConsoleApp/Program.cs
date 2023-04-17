2b0c04209df94bef9a370aeb8ac3a5c74de2ce06
Stefan Vettiger <vettiger.stefan@gmail.com>
2023-04-17 20:43:29 +0200

    improved

--- HashPayloadConsoleApp/Program.cs
+++ HashPayloadConsoleApp/Program.cs
@@ -7,30 +7,41 @@ namespace HashPayloadConsoleApp
     {
         static void Main(string[] args)
         {
-            string payload = "Hello, world!"; // Your arbitrary payload
+            string payload = """
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
+                tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, 
+                no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, 
+                consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore 
+                magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo 
+                dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                """;
             ECParameters publicKeyParameters;
             byte[] payloadBytes;
             string base64SignatureBytes;
 
-            Console.WriteLine($"Original Payload: {payload}");
+            Console.WriteLine($"Original Payload:");
+            Console.WriteLine();
+            Console.WriteLine($"{payload}");
 
-            // Generate a new RSA key pair
-            using (var rsa = ECDsa.Create(ECCurve.NamedCurves.brainpoolP160r1))
+            // Generate a new key pair
+            using (ECDsa asymmetricAlgorithm = ECDsa.Create(ECCurve.NamedCurves.brainpoolP160r1))
             {
                 // Get the public and private key as byte arrays
-                publicKeyParameters = rsa.ExportParameters(false);
+                publicKeyParameters = asymmetricAlgorithm.ExportParameters(false);
 
                 // Convert the payload to a byte array
                 payloadBytes = Encoding.UTF8.GetBytes(payload);
 
-                // Compute the hash of the payload using SHA256
+                // Compute the hash of the payload
                 byte[] hashBytes = GetPayloadHash(payloadBytes);
 
                 // Sign the hash using the private key
-                byte[] signatureBytes = rsa.SignHash(hashBytes);
-                                
+                byte[] signatureBytes = asymmetricAlgorithm.SignHash(hashBytes);
+
                 base64SignatureBytes = Convert.ToBase64String(signatureBytes);
-                Console.WriteLine(base64SignatureBytes);
+                Console.WriteLine();
+                Console.WriteLine($"{base64SignatureBytes}    lenght: {base64SignatureBytes.Length}");
             }
 
             // change payload
@@ -49,14 +60,20 @@ namespace HashPayloadConsoleApp
 
         private static byte[] GetPayloadHash(byte[] payloadBytes)
         {
-            return MD5.HashData(payloadBytes);
+            byte[] hashBytes;
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                hashBytes = sha256.ComputeHash(payloadBytes);
+            }
+
+            return hashBytes;
         }
 
         private static void VerifyHash(byte[] payloadBytes, string base64SignatureBytes, ECParameters parameters)
         {
             using (var rsa = ECDsa.Create(parameters))
             {
-                // Compute the hash of the payload using SHA256
+                // Compute the hash of the payload
                 byte[] hashBytes = GetPayloadHash(payloadBytes);
 
                 // verify the signature using the public key
@@ -64,9 +81,12 @@ namespace HashPayloadConsoleApp
                 bool signaturevalid = rsa.VerifyHash(hashBytes, signatureBytes);
 
                 Console.WriteLine("signature is valid: {0}", signaturevalid);
+                Console.WriteLine();
 
                 var payload = Encoding.UTF8.GetString(payloadBytes);
-                Console.WriteLine($"Received Payload: {payload}");
+                Console.WriteLine($"Received Payload:");
+                Console.WriteLine();
+                Console.WriteLine($"{payload}");
                 Console.Read();
             }
         }
